18142945434016678d3dc55a4b29986dab2b70be
jet <artturi@juvonen.eu>
2023-02-08 20:17:40 +0200

    unfinished p.528 implementation

--- rxt.m
+++ rxt.m
@@ -1,3 +1,8 @@
+clear all
+close all
+clc
+ignore_function_time_stamp ("all")
+
 % Ambient parameters
 global c; c = 299792458;          % [m/s]  Speed of light
 global k; k = 1.38064852*10^-23;  % [J/K]  Bolzmann's constant
@@ -122,25 +127,27 @@ function calc_node_geodist_alts
 endfunction
 
 % Calculate node received powers using ITU-R P.528-5 implementation by Ivica Stevanovic https://doi.org/10.5281/zenodo.6984262
-function p528_result = calc_node_rx_pwr_p528
-  disp("Calculating node received powers using free space loss...")
+function calc_node_rx_pwr_p528
+  disp("Calculating node received powers using ITU-R P.528-5...")
   tic
   calc_node_geodist_alts;
   global node_geodist_alts;
-  global node_tx_pwr;
-  global node_rx_pwr;
+  % global node_tx_pwr;
+  % global node_rx_pwr;
   global f;
   global q;
-  p528_result = zeros(length(node_geodist_alts),length(node_geodist_alts));
 
+  % p528_result = zeros(length(node_geodist_alts),length(node_geodist_alts));
+  % p528_result(1,2) = tl_p528(node_geodist_alts(1,2,1),min(node_geodist_alts(1,2,2:3)),max(node_geodist_alts(1,2,2:3)),f,0,q).A__db;
+
+  p528_result = zeros(length(node_geodist_alts),length(node_geodist_alts));
   for i = 1:length(node_geodist_alts)
     for j = 1:length(node_geodist_alts)
       p528_result(i,j) = tl_p528(node_geodist_alts(i,j,1),min(node_geodist_alts(i,j,2:3)),max(node_geodist_alts(i,j,2:3)),f,0,q).("A__db");
-    end
-  end
+    endfor
+  endfor
 
-  node_rx_pwr = -1*p528_result;
-  %node_rx_pwr = -1*p528_result - watt2dbm(node_tx_pwr);
+  node_rx_pwr = -1*p528_result - watt2dbm(node_tx_pwr);
   toc
 endfunction
 
