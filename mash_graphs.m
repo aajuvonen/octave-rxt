19a7d06c9a362f2c68dbdd520514cc91ffa12bdc
jet <artturi@juvonen.eu>
2023-02-10 14:42:14 +0200

    add graph combining routine mash_graphs.m

---
+++ mash_graphs.m
@@ -0,0 +1,6 @@
+% Combine multiple graphs to a single graph
+function mash_graphs(A,B)
+  graph_mashed = cat(2,A,zeros(rows(A),columns(B)));
+  graph_mashed = cat(1,graph_mashed,[zeros(rows(B),columns(A)) B]);
+  disp("   Combined adjacency matrix generated to 'graph_mashed")
+endfunction
