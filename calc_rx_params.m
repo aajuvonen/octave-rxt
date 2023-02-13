288cd17bbba92dfbc54b29d9d5f8f52d633bb747
jet <artturi@juvonen.eu>
2023-02-13 22:46:53 +0200

    rename Bolzmann's constant from k to k_B

--- calc_rx_params.m
+++ calc_rx_params.m
@@ -1,7 +1,7 @@
 % Calculate receiver parameters
 function calc_rx_params
   globals;
-  N_ktb = k*T_0*B;
+  N_ktb = k_B*T_0*B;
   N_0 = watt2dbm(N_ktb);
   N_tot = N_0 + N_F + L_sf;
   S_min = N_tot + SNR_req;
