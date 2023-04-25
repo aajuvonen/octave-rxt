0710931dc033e2fa2e93650ab7583e91b4a277a9
jet <artturi@juvonen.eu>
2023-04-25 22:14:07 +0300

    minor fix to model_p1238 warnings, change path loss to negative instead of positive values

--- model_p1238.m
+++ model_p1238.m
@@ -17,8 +17,8 @@ function path_loss_p1238 = model_p1238(param_dist,param_los = 1,param_env = 1)
     if(warnings)
       if(f < loss_freqs(coeff_row,1)) printf("Warning: frequency lower than model minimum %d", loss_freqs(coeff_row,1)), disp(" MHz") endif
       if(f > loss_freqs(coeff_row,2)) printf("Warning: frequency exceeds model maximum %d", loss_freqs(coeff_row,2)), disp(" MHz") endif
-      if(param_dist < loss_freqs(coeff_row,3)) printf("Warning: distance lower than model minimum %d", loss_freqs(coeff_row,1)), disp(" m") endif
-      if(param_dist > loss_freqs(coeff_row,4)) printf("Warning: distance exceeds model maximum %d", loss_freqs(coeff_row,2)), disp(" m") endif
+      if(param_dist < loss_freqs(coeff_row,3)) printf("Warning: distance lower than model minimum %d", loss_freqs(coeff_row,3)), disp(" m") endif
+      if(param_dist > loss_freqs(coeff_row,4)) printf("Warning: distance exceeds model maximum %d", loss_freqs(coeff_row,4)), disp(" m") endif
     endif
 
     % Loss coefficients
@@ -28,5 +28,5 @@ function path_loss_p1238 = model_p1238(param_dist,param_los = 1,param_env = 1)
     sigma = loss_coeff(coeff_row,4);
 
     % Eq. (1), with added factor sigma
-    path_loss_p1238 = 10 * alpha .* log10(param_dist) + beeta + 10 * gamma * log10(f) + norm_inv(q) * sigma;
+    path_loss_p1238 = -(10 * alpha .* log10(param_dist) + beeta + 10 * gamma * log10(f) + norm_inv(q) * sigma);
 endfunction
