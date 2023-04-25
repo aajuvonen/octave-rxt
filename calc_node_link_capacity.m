966f3e493e85529c0ba1d0641131963ea3018a05
jet <artturi@juvonen.eu>
2023-04-25 21:52:04 +0300

    add calc_node_link_capacity routine

---
+++ calc_node_link_capacity.m
@@ -0,0 +1,9 @@
+% Calculate node link channel capacity
+
+function calc_node_link_capacity
+  disp("Calculating node link channel capacities... ")
+  ticstart
+  globals
+    node_link_capacity = B * log2(1 + dbm2watt(node_rx_pwr - N_tot));
+  ticstop
+endfunction
