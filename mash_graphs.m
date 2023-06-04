3b3170834b9a089c646d01dccf8507b11f6d7151
jet <artturi@juvonen.eu>
2023-06-04 23:09:03 +0300

    new routine slice_graphs.m, slight modifications to mash_graphs

--- mash_graphs.m
+++ mash_graphs.m
@@ -5,7 +5,8 @@
 % Output:   graph_mashed  Adjacency matrix of the combined graph
 
 function graph_mashed = mash_graphs(graph_a,graph_b)
-  graph_mashed = cat(2,A,zeros(rows(graph_a),columns(graph_b)));
-  graph_mashed = cat(1,graph_mashed,[zeros(rows(graph_b),columns(graph_a)) graph_b]);
+  globals
+    graph_mashed = cat(2,A,zeros(rows(graph_a),columns(graph_b)));
+    graph_mashed = cat(1,graph_mashed,[zeros(rows(graph_b),columns(graph_a)) graph_b]);
   if(hints) disp("   Combined adjacency matrix generated to 'graph_mashed") endif
 endfunction
