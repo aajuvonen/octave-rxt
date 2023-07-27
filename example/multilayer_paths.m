d0100c428f87dfb09c1b08f63bcd6db48e6a00fb
jet <artturi@juvonen.eu>
2023-07-27 22:56:19 +0300

    fix breaking operator bug in model_hata_mod.m, develop multilayer_paths.m, minor changes to aeronautical_interference.m

--- example/multilayer_paths.m
+++ example/multilayer_paths.m
@@ -3,6 +3,14 @@ disp("   Artturi Juvonen 2023")
 disp("   artturi@juvonen.eu")
 disp("")
 
+## Ambient parameters
+T_0 = 270;                              # [K]    Ambient temperature
+QNH = 961;                              # [hPa]  Atmospheric pressure
+q = 50;                                 # [%]    Channel time availability
+L_sf_std__dB = 9;                       # [dB]   Path loss variation standard deviation
+L_sf__dB = L_sf_std__dB * norm_inv(q);  # [dB]   Path loss variation for the corresponding time availability
+calc_k_factor;                          # Calculate Earth effective radius factor K
+
 ## Receiver parameters simulating TETRA
 f__MHz = 400;                           # [MHz]  Transceiver frequency
 B__Hz = 25e3;                           # [Hz]   Transceiver bandwidth
@@ -12,17 +20,18 @@ calc_rx_params;                         # Calculate receiver parameters
 
 ## Node 3D-coordinates
 node_xyz = [20.194,14.617,0.002;12.872,32.761,0.010;37.279,19.865,0.006;24.941,21.819,0.005;22.575,32.132,0.001;11.233,27.961,0.010;24.818,17.711,0.010;25.788,27.591,0.002;21.888,27.870,0.002;13.139,17.800,0.007;13.135,37.876,0.004;23.435,36.312,0.003;13.962,21.043,0.009;31.935,12.145,0.009;27.045,31.655,0.008;12.707,22.606,0.008;16.360,21.629,0.008;31.502,17.950,0.007;30.289,39.787,0.005;29.274,27.727,0.009];
+% node_xyz = [61.556,53.945,0.001;66.410,53.744,0.005;43.017,94.629,0.008;18.223,82.636,0.002;73.556,32.049,0.003;23.531,21.165,0.005;96.940,10.892,0.004;88.237,97.749,0.004;12.046,50.171,0.010;33.356,88.881,0.008;80.452,16.080,0.008;77.427,89.386,0.008;10.562,50.680,0.002;46.283,85.974,0.003;64.035,27.566,0.005;95.590,61.273,0.003;12.511,50.124,0.005;23.594,43.101,0.003;16.671,42.263,0.007;58.568,59.708,0.001];
 
 ## Number of transceivers
-node_count = length(node_xyz);
+node_count = rows(node_xyz);
 
 ## Node EIRP [W]
 node_tx_pwr(:,1) = [25;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1;1];
 
 ## Node transmittance status [bool]
-node_tx_pwr(:,2) = ones(1, length(node_tx_pwr));
+node_tx_pwr(:,2) = ones(1, rows(node_tx_pwr));
 
-calc_node_path_loss_hata_mod;           # Calculate path losses using itaero model
+calc_node_path_loss_hata_mod(1, 0.5);   # Calculate path losses using modified Hata model
 calc_node_rx_pwr;                       # Calculate node received powers
 calc_node_cnr;                          # Calculate node carrier-to-noise ratios
 
--- model_hata_mod.m
+++ model_hata_mod.m
@@ -14,6 +14,7 @@ function L_hata__dB = model_hata_mod(d__km, h_1__meter, h_2__meter, param_u = 0,
   globals
 
   if(warnings)
+    if(d__km < 1) disp("Warning: distance lower than model minimum 1 km") endif
     if(d__km > 100) disp("Warning: distance exceeds model maximum 100 km") endif
     d_hor = calc_d_hor(h_1__meter, h_2__meter);
     if(d__km > d_hor) disp("Warning: distance exceeds radio horizon") endif
@@ -27,23 +28,27 @@ function L_hata__dB = model_hata_mod(d__km, h_1__meter, h_2__meter, param_u = 0,
     if(f__MHz > 3000) disp("Warning: frequency exceeds model maximum 3000 MHz") endif
   endif
   
-    a_m = (0.7 - 1.1 * log10(f__MHz)) .* h_2__meter + 1.56 * log10(f__MHz) - 0.8;  # Eq. (7.12)
-    a_2 = 1.1 - 8.29 * log10(1.54 .* h_2__meter).^2;                               # Eq. (7.13)
-    a_4 = 4.97 - 3.2 * log10(11.75 .* h_2__meter).^2;                              # Eq. (7.14)
-    L_ps = -2 * log10(f__MHz / 28)^2 - 5.4;                                        # Eq. (7.15)
-    L_po = -4.78 * log10(f__MHz)^2 + 18.33 * log10(f__MHz) - 40.94;                # Eq. (7.16)
-    F_1 = (300^4) / (f__MHz^4 + 300^4);                                            # Eq. (7.17)
-    F_2 = (f__MHz^4) / (300^4 + f__MHz^4);                                         # Eq. (7.18)
-    S_ks_1 = 27 + (f__MHz/230);
-    S_ks_2 = 17 .* (h_1__meter + 20);
-    S_ks_3 = 17 .* (h_1__meter + 20) + d__km.^2;
-    S_ks_4 = 1.3 - (abs(f__MHz - 55) / 750);
-    S_ks = S_ks_1 * log10(S_ks_2 / S_ks_3) + S_ks_4;                               # Eq. (7.19)
-    S_o = (1-param_u_r) * ((1-2*param_u_r) * L_po + 4*param_u_r * L_ps);           # Eq. (7.20)
-    a_x = (1-param_u) * a_m + param_u * (a_2 * F_1 + a_4 * F_2);                   # Eq. (7.21)
-    B_o = 25 * log10(param_b) - 30;                                                # Eq. (7.22)
-    L_ccir  = 69.55 + 26.16 * log10(f__MHz) - 13.82 .* log10(h_1__meter);
-    L_ccir += (44.9 - 6.55 .* log10(h_1__meter)) .* log10(d__km);
-    L_ccir += a_x;                                                                 # Eq. (7.11)
-    L_hata__dB = -(L_ccir + S_o - S_ks + B_o);                                     # Eq. (7.23)
+    if(d__km == 0)
+      L_hata__dB = Inf;
+    else
+      a_m = (0.7 - 1.1 * log10(f__MHz)) .* h_2__meter + 1.56 * log10(f__MHz) - 0.8;  # Eq. (7.12)
+      a_2 = 1.1 - 8.29 * log10(1.54 .* h_2__meter).^2;                               # Eq. (7.13)
+      a_4 = 4.97 - 3.2 * log10(11.75 .* h_2__meter).^2;                              # Eq. (7.14)
+      L_ps = -2 * log10(f__MHz / 28)^2 - 5.4;                                        # Eq. (7.15)
+      L_po = -4.78 * log10(f__MHz)^2 + 18.33 * log10(f__MHz) - 40.94;                # Eq. (7.16)
+      F_1 = (300^4) / (f__MHz^4 + 300^4);                                            # Eq. (7.17)
+      F_2 = (f__MHz^4) / (300^4 + f__MHz^4);                                         # Eq. (7.18)
+      S_ks_1 = 27 + (f__MHz/230);
+      S_ks_2 = 17 .* (h_1__meter + 20);
+      S_ks_3 = 17 .* (h_1__meter + 20) + d__km.^2;
+      S_ks_4 = 1.3 - (abs(f__MHz - 55) / 750);
+      S_ks = S_ks_1 * log10(S_ks_2 ./ S_ks_3) + S_ks_4;                              # Eq. (7.19)
+      S_o = (1-param_u_r) * ((1-2*param_u_r) * L_po + 4 * param_u_r * L_ps);         # Eq. (7.20)
+      a_x = (1-param_u) * a_m + param_u * (a_2 * F_1 + a_4 * F_2);                   # Eq. (7.21)
+      B_o = 25 * log10(param_b) - 30;                                                # Eq. (7.22)
+      L_ccir  = 69.55 + 26.16 * log10(f__MHz) - 13.82 .* log10(h_1__meter);
+      L_ccir += (44.9 - 6.55 .* log10(h_1__meter)) .* log10(d__km);
+      L_ccir += a_x;                                                                 # Eq. (7.11)
+      L_hata__dB = -(L_ccir + S_o - S_ks + B_o);                                     # Eq. (7.23)
+    endif
 endfunction
