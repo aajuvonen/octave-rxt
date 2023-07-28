f6b04c64f040d2c4b13db597c341b60821e0b9fe
jet <artturi@juvonen.eu>
2023-07-28 22:47:33 +0300

    changes to plot_graph routine, due to addition of a new routine draw_graph_node_logical.m

---
+++ draw_graph_node_logical.m
@@ -0,0 +1,26 @@
+## Graph node logical
+##
+## Input:   param_graph         arr.  Adjacency matrix
+##
+## Output:  graph_node_logical  arr.  Adjacency matrix
+
+function draw_graph_node_logical(param_graph)
+  disp("Graphing node logical view... ")
+  ticstart
+  globals
+
+    ## Initialize an empty graph for logical connections and find physical layer's strongly connected components
+    graph_node_logical = logical(zeros(node_count, node_count));
+    find_cliques(param_graph);
+
+    ## Loop through node cliques and populate the logical graph accordingly
+    for i = 1:node_clique_count
+      for j = eval(['node_clique_' num2str(i)])
+        graph_node_logical(j, eval(['node_clique_' num2str(i)])) = 1;
+      endfor
+    endfor
+
+  ticstop
+  if(hints) disp("   Node logical adjacency matrix generated to 'graph_node_logical'") endif
+  if(hints) disp("   Call 'plot_graph(graph_node_logical, \"logical\")' to plot graph") endif
+endfunction
--- plot_graph.m
+++ plot_graph.m
@@ -1,12 +1,27 @@
 ## Function for plotting graphs
+##
+## Inputs:  param_graph  arr.  Adjacency matrix
+##          param_type   str.  (empty / phy / log)
+##
+## Output:  Graphical representation
 
-function plot_graph(param_graph)
+function plot_graph(param_graph, param_type = "phy")
   disp("Plotting graph...")
   ticstart
   globals
-    nodes = node_xyz(:,1:2);                           # Get node xy list
-    edges = getEdges(param_graph,'adjacency')(:,1:2);  # Get edge list
-    labels = [1:node_count]';                          # Get node numeric labels
-    plot_worker                                        # Call plot worker function
+    ## Plot physical graph (default)
+    if(param_type == "phy")
+      nodes = node_xyz(:,1:2);                           # Get node xy list
+      edges = getEdges(param_graph,'adjacency')(:,1:2);  # Get edge list
+      labels = [1:node_count]';                          # Get node numeric labels
+      plot_worker                                        # Call plot worker function
+
+    ## Plot logical graph (usually AFTER a physical graph was plotted)
+    elseif(param_type == "log")
+      gplot(graph_node_logical, [node_xyz(:,1,1) node_xyz(:,2,1)], "m");
+      axis("auto")
+      axis("off")
+    endif
+
   ticstop
 endfunction
