a399338c35b9bde1aff9fdd069a145bbac63a4e8
jet <artturi@juvonen.eu>
2023-07-29 11:55:08 +0300

    comments and cleanup for plotting routines

--- plot_graph.m
+++ plot_graph.m
@@ -16,16 +16,16 @@ function plot_graph(param_graph_phy, param_graph_log)
     endif
 
     ## Plot physical graph (default)
-    nodes = node_xyz(:,1:2);                                # Get node xy list
-    edges = getEdges(param_graph_phy, 'adjacency')(:,1:2);  # Get edge list
-    labels = [1:node_count]';                               # Get node numeric labels
-    plot_worker("phy")                                      # Call plot worker function
+    nodes = node_xyz(:,1:2);                                  # Get node xy list
+    edges = getEdges(param_graph_phy, 'adjacency')(:,1:2);    # Get edge list
+    labels = [1:node_count]';                                 # Get node numeric labels
+    plot_worker("phy")                                        # Call plot worker function
 
     ## Plot physical and logical graph
     if(exist("param_graph_log"))
-      subplot(1,2,2)
+      subplot(1,2,2)                                          # Use subplot 2 for logical layer
       edges = getEdges(param_graph_log, 'adjacency')(:,1:2);  # Get edge list
-      plot_worker("log")
+      plot_worker("log")                                      # Call plot worker function
     endif
 
   ticstop
--- plot_worker.m
+++ plot_worker.m
@@ -4,25 +4,25 @@ function plot_worker(param_type = "phy")
   globals
 
     if(param_type == "phy")
-      figure(1, "position", [320,240,512,512]);
+      figure(1, "position", [320,240,512,512]);            # Set figure dimensions
       plot(nodes(:,1), nodes(:,2), ".k", "markersize",8);  # Draw nodes
       drawDirectedEdges(nodes, edges);                     # Draw directed edges
 
     elseif(param_type == "log")
-      figure(1, "position", [320,240,1024,512]);
-      subplot(1,2,2)
+      figure(1, "position", [320,240,1024,512]);           # Set figure dimensions
+      subplot(1,2,2)                                       # Use subplot 2 for logical layer 
       drawGraphEdges(nodes, edges);                        # Draw undirected edges
     endif
 
-    plot_labels = drawNodeLabels(nodes, labels);         # Draw node labels
-    set(plot_labels, "fontsize", 12);                    # Resize node label font
+    plot_labels = drawNodeLabels(nodes, labels);           # Draw node labels
+    set(plot_labels, "fontsize", 12);                      # Resize node label font
 
+    ## Set plot axis value
     axisscale = 10 ^ length(num2str(ceil((max(max(node_xyz))))) - 1) / 2;
-    axisval = ceil((max(max(node_xyz))) / axisscale) * axisscale;        # Get plot axis value
-    % axisval = ceil((max(max(node_xyz))) / 50) * 50;        # Get plot axis value
-
-    axis([0 axisval 0 axisval], "equal");                  # Set plot axes
-
+    axisval = ceil((max(max(node_xyz))) / axisscale) * axisscale;        
+    
+    ## Set plot axes
+    axis([0 axisval 0 axisval], "equal");                  
     if(param_type == "log")
       axis("off")
     endif
