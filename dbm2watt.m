22c5c4e7efd66c485e74aa8d13c537f57c921ea7
jet <artturi@juvonen.eu>
2023-07-29 13:26:51 +0300

    fix breaking variable name bug in dbm2watt.m

--- dbm2watt.m
+++ dbm2watt.m
@@ -5,5 +5,5 @@
 ## Output: retval__watt  Watt  [W]
 
 function retval__watt = dbm2watt(val__dBm)
-  retval_watt = 10.^(val__dBm / 10) / 1000;
+  retval__watt = 10.^(val__dBm / 10) / 1000;
 endfunction
