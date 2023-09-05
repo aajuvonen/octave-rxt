456479208dbe9bb71f418612efa9eb21521434b4
jet <artturi@juvonen.eu>
2023-09-05 13:45:28 +0300

    fix variable typo in model_overhor.m

--- model_overhor.m
+++ model_overhor.m
@@ -16,7 +16,7 @@ function L_overhor__dB = model_overhor(d__km, h_1__meter, h_2__meter)
   factor1 = -20 * log10((h_1__meter * h_2__meter) .\ (d__km * 1000).^2);
   factor2 = -10 * log10((1 + (d__km / (6 * d_hor)).^ 7) / 1 +(d__km / d_hor).^ 3);
   factor3 = -d__km ./ (13 + 77 * (d_hor ./ d__km));
-  factor4 = (22 + (f / 2000)) * log10(100 / f);
+  factor4 = (22 + (f__MHz / 2000)) * log10(100 / f__MHz);
 
   L_overhor__dB = factor1 + factor2 + factor3 + factor4;
 endfunction
