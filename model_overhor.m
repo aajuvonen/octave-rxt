48081e118ff82820ae9f3a614da69c31d25662c6
jet <artturi@juvonen.eu>
2023-09-05 13:52:13 +0300

    fix array error in model_overhor.m

--- model_overhor.m
+++ model_overhor.m
@@ -13,8 +13,8 @@ function L_overhor__dB = model_overhor(d__km, h_1__meter, h_2__meter)
   d_hor = calc_d_hor(h_1__meter, h_2__meter);
 
   ## Eq. (7.25)
-  factor1 = -20 * log10((h_1__meter * h_2__meter) .\ (d__km * 1000).^2);
-  factor2 = -10 * log10((1 + (d__km / (6 * d_hor)).^ 7) / 1 +(d__km / d_hor).^ 3);
+  factor1 = -20 * log10((h_1__meter .* h_2__meter) .\ (d__km * 1000).^2);
+  factor2 = -10 * log10((1 + (d__km ./ (6 * d_hor)).^ 7) / 1 + (d__km ./ d_hor).^ 3);
   factor3 = -d__km ./ (13 + 77 * (d_hor ./ d__km));
   factor4 = (22 + (f__MHz / 2000)) * log10(100 / f__MHz);
 
